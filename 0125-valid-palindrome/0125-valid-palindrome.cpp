e71b7feca664a16707c9556f73f80fe6a7b55597
jeontigger <boy9609@naver.com>
2023-10-12 14:11:30 +0900

    Time: 7 ms (38.01%), Space: 7.8 MB (21.73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,18 +4,20 @@ public:
         string str = "";
 
         for(int i = 0 ; i<s.length() ; i++){
-            if(s[i] >= '0' && s[i] <='9'){
+            if(isdigit(s[i])){
                 str+= s[i];
             }
-            if(tolower(s[i]) >= 'a' && tolower(s[i]) <= 'z'){
-                str+=tolower(s[i]);
+            if(isalpha(s[i])){
+                str+= tolower(s[i]);
             }
         }
 
         if(str ==""){
             return true;
         }
+
         cout << str << endl;
+        
         for(int i = 0; i<str.length()/2+1 ; i++){
             if(str[i]!=str[str.length()-1-i]){
                 return false;
