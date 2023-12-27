79e7fa8feb95ab319b43772a8a0f32338703d0df
jeontigger <boy9609@naver.com>
2023-12-27 11:40:07 +0900

    Time: 35 ms (11.10%), Space: 27.9 MB (24.84%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,10 +1,39 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int solution1(vector<int>& nums, int target){
         auto iter = find(nums.begin(), nums.end(), target);
         if(iter == nums.end()){
             return -1;
         }
         return iter - nums.begin();
+    }
+
+    int solution2(vector<int>& nums, int target){
+        int left = 0;
+        int right = nums.size()-1;
+        while(true){
+            int idx = (right + left) / 2;
+            int num = nums[idx];
+            cout << idx << " " << num << endl;
+            if(num < target){
+                left = idx+1;
+            }else if(target < num){
+                right = idx - 1;
+            }else{
+                return idx;
+            }
+            
+            if(right < left){
+                return -1;
+            }
+        }
+        return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int res = 0;
+        // res = solution1(nums, target);
+        res = solution2(nums, target);
+
+        return res;
     }
 };
