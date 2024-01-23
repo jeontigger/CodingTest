e9f88e16df300ec2ed21561a7d0c1ba69e213d50
jeontigger <boy9609@naver.com>
2024-01-23 17:10:43 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 8 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.cc
@@ -1,3 +1,7 @@
+#pragma once
+// CondingTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
 #include <iostream>
 #include <vector>
 #include <list>
@@ -12,51 +16,58 @@ void input(int& num) {
     cin >> num;
 }
 
+bool isPalin(int num) {
+    string str = to_string(num);
+    size_t strLen = str.length();
+
+    for (size_t j = 0; j <= strLen / 2; j++)
+    {
+        if (str[j] != str[strLen - 1 - j]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool isPrime(int num) {
+
+    for (size_t i = 2; i <= sqrt(num); i++)
+    {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 void solution(int num) {
 
     // num 이상인 수 && 소수 && 팰린드롬
 
     // 소수 판별
         // 2부터 sqrt(n)까지
-    
-    if(num <= 1){
+    if (num <= 1) {
         num = 2;
+    }
+    if (num == 2) {
+    cout << 2;
+    return;
+}
+
+    if (num % 2 == 0) {
+        num++;
     }
 
     while (true) {
 
-        bool isPrime = true;
-        for (size_t i = 2; i <= sqrt(num); i++)
-        {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
+        if (isPalin(num) && isPrime(num)) {
+            cout << num << endl;
+            break;
         }
-
-        // 소수라면 팰린드롬 판별
-        if (isPrime) {
-            string str = to_string(num);
-
-            size_t strLen = str.length();
-
-            bool isPalindrome = true;
-            for (size_t j = 0; j <= strLen/2; j++)
-            {
-                if (str[j] != str[strLen - 1 - j]) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-
-            if (isPalindrome) {
-                cout << num << endl;
-                break;
-            }
-
-        }
-
-        num++;
+       
+        num+=2;
         if (num >= 100000000)
         {
             cout << "OVER" << endl;
@@ -68,8 +79,10 @@ void solution(int num) {
 
 int main()
 {
-    int num;
+    vector<vector<int>> grid;
 
+    int num;
     input(num);
     solution(num);
+
 }
