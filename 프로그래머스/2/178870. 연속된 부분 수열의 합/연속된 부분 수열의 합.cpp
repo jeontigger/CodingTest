798248cfb7d070a01d225554b79b233dff7af6d0
jeontigger <boy9609@naver.com>
2024-09-30 09:02:15 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 6.06 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.cpp
@@ -6,30 +6,40 @@ using namespace std;
 
 vector<int> solution(vector<int> sequence, int k) {
     vector<int> answer;
-    answer.push_back(0);
-    answer.push_back(sequence.size());
     
-    int leftIdx = 0;
-    int rightIdx = 0;
-    
-    int sum = sequence[0];
-    while(leftIdx <= rightIdx){
-        if(sum < k){
-            rightIdx++;
-            if(rightIdx == sequence.size())
-                break;
-            sum += sequence[rightIdx];
-        }else if (sum > k){
-            sum -= sequence[leftIdx];
-            leftIdx++;
-        }else{
-            if(answer[1]-answer[0] > rightIdx - leftIdx){
-                answer[1] = rightIdx;
-                answer[0] = leftIdx;
+    int left = 0, right = 0;
+    int curSum = sequence[0];
+    while(left <= right){
+        
+        if(curSum == k){
+            if(answer.empty()){
+                answer.push_back(left);
+                answer.push_back(right);
+            }else if(answer[1] - answer[0] > right-left){
+                answer.clear();
+                answer.push_back(left);
+                answer.push_back(right);
             }
-            sum -= sequence[leftIdx];
-            leftIdx++;
+            // cout << curSum << " " << left << " " << right<<endl;
+            curSum -= sequence[left];
+            left++;
+            continue;
         }
+        // cout << curSum << " " << left << " " << right<<endl;
+        if(curSum < k){
+            right++;
+            curSum += sequence[right];
+            if(right == sequence.size()){
+                break;
+            }
+            continue;
+        }
+        
+        if(curSum > k){
+            curSum -= sequence[left];
+            left++;
+        }
+        
     }
     return answer;
 }
