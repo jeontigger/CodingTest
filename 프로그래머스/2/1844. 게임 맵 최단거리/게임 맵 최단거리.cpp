054c663efbe5ec08f90a09cb0ee8c7530d64f5a2
jeontigger <boy9609@naver.com>
2024-10-10 10:39:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.22 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,82 +1,38 @@
-#include<vector>
-#include<iostream>
+#include <vector>
 #include <queue>
 using namespace std;
 
-#define MAX 2100000000
-int g_minPath = MAX;
-
-int g_row[4] = {0, 1, 0, -1};
-int g_col[4] = {1, 0, -1, 0};
-
-int g_targetRow = 0;
-int g_targetCol = 0;
-
-void SearchPath_DFS(int row, int col, int cnt, vector<vector<bool>>& visited, const vector<vector<int>>& maps){
-    // 더 갈 필요 없는 조건
-    if(cnt >= g_minPath){
-        return;
-    }
-    
-    // 종료 조건
-    if(row == maps.size()-1 && col == maps[0].size()-1){
-        g_minPath = min(g_minPath, cnt);
-        return;
-    }
-    
-    // 다음 수행
-    for(int i =0; i<4;i++){
-        int nRow = row + g_row[i];
-        int nCol = col + g_col[i];
-        
-        // 범위 체크
-        if(nRow >= 0 && nRow < maps.size() && nCol >= 0 && nCol < maps[0].size()){
-            // 길 체크
-            if(maps[nRow][nCol] == 0){
-                continue;
-            }
-            // 방문체크
-            if(!visited[nRow][nCol]){
-                visited[nRow][nCol] = true;
-                SearchPath_DFS(nRow, nCol, cnt+1, visited, maps);
-                visited[nRow][nCol] = false;
-            }
-        }
-    }
-}
-
-struct Pos{
+struct Data{
     int row;
     int col;
     int cnt;
 };
 
-int SearchPath_BFS(vector<vector<int>>& maps){
-    queue<Pos> q;
-    
+int g_nrow[] = {0, 1, 0, -1};
+int g_ncol[] = {1, 0, -1, 0};
+
+int solution(vector<vector<int>> maps)
+{
+    int answer = 0;
+    queue<Data> q;
     q.push({0, 0, 1});
-    
-    int maxRow = maps.size();
-    int maxCol = maps[0].size();
+    maps[0][0] = 0;
     
     while(!q.empty()){
-        auto data = q.front();
+        Data data = q.front();
         q.pop();
         
-        // 도달(종료조건)
-        if(data.row ==maxRow -1 && data.col == maxCol -1){
+        if(data.row == maps.size()-1 && data.col == maps[0].size()-1){
             return data.cnt;
         }
         
-        for(int i = 0; i < 4; i++){
-            int nrow = data.row + g_row[i];
-            int ncol = data.col + g_col[i];
+        for(int i = 0; i< 4; i++){
+            int nrow = data.row + g_nrow[i];
+            int ncol = data.col + g_ncol[i];
             
-            // 범위 체크
-            if(nrow >= 0 && nrow < maxRow && ncol >= 0 && ncol < maxCol){
-                // 벽 체크
-                if(maps[nrow][ncol]){
-                    q.push({nrow, ncol, data.cnt + 1});
+            if(0 <= nrow && nrow < maps.size() && 0 <= ncol && ncol < maps[0].size()){
+                if(maps[nrow][ncol] == 1){
+                    q.push({nrow, ncol, data.cnt+1});
                     maps[nrow][ncol] = 0;
                 }
             }
@@ -86,17 +42,12 @@ int SearchPath_BFS(vector<vector<int>>& maps){
     return -1;
 }
 
-int solution(vector<vector<int> > maps)
-{
-    int answer = 0;
-    vector<vector<bool>> visited;
-    for(int i =0; i < maps.size();i++){
-        vector<bool> v(maps[i].size());
-        visited.push_back(v);
-    }
-    
-    // SearchPath_DFS(0, 0, 0, visited, maps);
-    answer = SearchPath_BFS(maps);
 
-    return answer;
-}
+
+
+
+
+
+
+
+
