ab87d38b5190d543f98b740e734261382135df6d
jeontigger <boy9609@naver.com>
2024-10-10 10:01:35 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -2,33 +2,42 @@
 #include <vector>
 #include <queue>
 
+struct Pos{
+    int row;
+    int col;
+};
+
+int g_nrow[] = {0, 1, 0, -1};
+int g_ncol[] = {1, 0, -1, 0};
+
 using namespace std;
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    int comCnt = computers.size();
-    vector<bool> visited(comCnt);
-    
+    vector<vector<int>> maps(computers);
+    vector<bool> visited(n);
     queue<int> q;
-    
-    for(int i =0; i<comCnt;i++){
-        if(!visited[i]){
-            q.push(i);
-            visited[i] = true;
-            answer++;
-        }
+
+    for(int i = 0 ; i < n ; i++){
+        if(visited[i]) continue;
+        
+        q.push(i);
+        visited[i] = true;
+        
+        answer++;
         
         while(!q.empty()){
-            int n = q.front();
+            int idx = q.front();
             q.pop();
             
-            for(int i = 0; i<comCnt; i++){
-                if(computers[n][i] && !visited[i]){
-                    q.push(i);
-                    visited[i] = true;
+            for(int j = 0 ; j < n; j++){
+                if(computers[idx][j] == 1 && !visited[j]){
+                    q.push(j);
+                    visited[j] = true;
                 }
             }
         }
     }
+    
     return answer;
 }
