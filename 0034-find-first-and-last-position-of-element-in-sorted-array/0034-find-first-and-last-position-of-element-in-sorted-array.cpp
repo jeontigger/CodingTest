b4c6b71d0e7a14013b275790cb540c929bc4eb94
jeontigger <boy9609@naver.com>
2024-10-15 08:29:56 +0900

    Time: 0 ms (100%), Space: 16.1 MB (92.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        
+        int left = 0, right = nums.size()-1;
+
         int targetIdx = 0;
 
-        if(nums.size()==0){
-            return {-1, -1};
-        }
-
+        if(nums.size() == 0) return {-1, -1};
+        
+        // find left side
         while(left <= right){
             int mid = (left + right) / 2;
 
             if(nums[mid] == target){
-                targetIdx = mid;
-                break;
-            }else if(nums[mid] < target){
                 left = mid + 1;
-            }else{
+                targetIdx = mid;
+            }else if(nums[mid] > target){
                 right = mid - 1;
+            }else{
+                left = mid + 1;
             }
         }
+        if(nums[targetIdx] != target) {return {-1, -1};}
+        int endIdx = targetIdx;
 
-        if(nums[targetIdx] != target){
-            return {-1, -1};
-        }
+        left = 0, right = nums.size()-1;
+        // find right side
+        while(left <= right){
+            int mid = (left + right) / 2;
 
-        int startIdx, endIdx;
-        for(int i = targetIdx; i >= 0; i--){
-            if(nums[i] != target)
-                break;
-            startIdx = i;
+            if(nums[mid] == target){
+                right = mid - 1;
+                targetIdx = mid;
+            }else if(nums[mid] > target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
         }
-        for(int i = targetIdx; i < nums.size(); i++){
-            if(nums[i] != target)
-                break;
-            endIdx = i;
-        }
+        int startIdx = targetIdx;
 
         return {startIdx, endIdx};
     }
