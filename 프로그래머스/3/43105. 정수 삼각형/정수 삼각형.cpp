d1ba169e264ab014d8a63bd85ae44f832424c2ad
jeontigger <boy9609@naver.com>
2024-10-21 09:34:24 +0900

    [level 3] Title: 정수 삼각형, Time: 0.35 ms, Memory: 8.04 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -1,34 +1,24 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<vector<int>> triangle) {
     int answer = 0;
-    vector<vector<int>> dp(triangle.size(), vector<int>(triangle.size()));
+    vector<int> dp(triangle.back().size());
+    dp[0] = triangle[0][0];
     
-    dp[0][0] = triangle[0][0];
-    dp[1][0] = triangle[1][0] + dp[0][0];
-    dp[1][1] = triangle[1][1] + dp[0][0];
-    
-    for(int i =2 ; i < triangle.size();i++){
-        dp[i][0] = triangle[i][0] + dp[i-1][0];
-        for(int j = 1; j < triangle[i].size();j++){
-            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+    for(int i = 1; i < triangle.size(); i++){
+        vector<int> origin = dp;
+        dp[0] = dp[0] + triangle[i][0];
+        
+        for(int j = 1 ; j < triangle[i].size(); j++){
+            dp[j] = max(origin[j-1], origin[j]) + triangle[i][j];
         }
     }
     
-    // for(auto& v:dp){
-    //     for(int i : v){
-    //         cout << i << " ";
-    //     }
-    //     cout << endl;
-    // }
-    
-    
-    answer = *max_element(dp.back().begin(), dp.back().end());
-    
+    answer = *max_element(dp.begin(), dp.end());
     return answer;
 }
