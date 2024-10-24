f285e7e72692843c895c3d7e63185f15e6bb9233
jeontigger <boy9609@naver.com>
2024-10-24 14:01:36 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,63 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int g_nrow[] = { 0, 1, 0, -1 };
+int g_ncol[] = { 1, 0, -1, 0 };
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> nums(N);
+	for (int i = 0; i < N; i++) {
+		cin >> nums[i];
+	}
+
+	long long sum = 0;
+	int left = 0;
+	int cnt = 0;
+	for (int right = 0; right < N; right++) {
+		sum += nums[right];
+		//cout << "right " << sum << " " << left << " " << right << endl;
+
+		while (sum >= M && left <= right) {
+			if (sum == M) cnt++;
+			sum -= nums[left];
+			//cout << "left " << sum << " " << left << " " << right << endl;
+			left++;
+		}
+
+	}
+
+	cout << cnt;
+
+	return 0;
+}
