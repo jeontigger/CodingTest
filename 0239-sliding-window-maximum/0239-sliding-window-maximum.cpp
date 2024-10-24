a8d9f084a7d1b33919605209c4ee0feb6c23b571
jeontigger <boy9609@naver.com>
2024-10-24 15:23:03 +0900

    Time: 75 ms (95.08%), Space: 152.7 MB (19.49%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        struct Cmp {
-            bool operator()(const pair<int, int> &a, const pair<int, int> &b)
-            {
-                if(a.first == b.first){
-                    return a.second > b.second;
-                }
-                return a.first < b.first;
-            };
-        };
+    struct Data{
+        int idx;
+        int val;
+    };
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, Cmp> pq;
-        
-
-        for(int i = 0; i<k;i++){
-            pq.push({nums[i], i});
+    struct cmp{
+        bool operator()(const Data& d1, const Data& d2){
+            return d1.val == d2.val ? d1.idx > d2.idx : d1.val < d2.val;
         }
-        vector<int>res;
+    };
 
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<Data, vector<Data>, cmp> pq;
 
-        for(int i = 0; i<nums.size() - k ; i++){
-            int curMax = pq.top().first;
-            res.push_back(curMax);
-            int prevNum = nums[i];
-            if(prevNum == curMax){
-                while(true){
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            pq.push({i, nums[i]});
+        }
+        res.push_back(pq.top().val);
+
+        for(int i = k ; i<nums.size(); i++){
+            pq.push({i, nums[i]});
+            int popNum = nums[i-k];
+
+            if(popNum == pq.top().val){
+                while(!pq.empty()){
                     pq.pop();
-                    if(pq.top().second >=i){
+                    if(pq.top().idx > i-k){
                         break;
                     }
                 }
             }
-            pq.push({nums[i+k], i+k});
+            res.push_back(pq.top().val);
         }
-        res.push_back(pq.top().first);
-
-        return res;    
+        return res;
     }
 };
