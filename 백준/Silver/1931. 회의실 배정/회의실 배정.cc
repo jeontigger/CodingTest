78aa315d71b8549a95b42ade8ac2dddfe272d029
jeontigger <boy9609@naver.com>
2024-10-25 15:55:23 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,62 +1,92 @@
-
-#include <iostream>
 #include <vector>
-#include <list>
+#include <iostream>
+#include <string>
 #include <algorithm>
 #include <queue>
+#include <cmath>
+#include <stack>
 
 using namespace std;
 
-void input(vector<pair<int, int>>& vec) {
-    int num;
+#define INF 1e9
 
-    cin >> num;
-    vec.resize(num);
-    for (int i = 0; i < num; i++) {
-        cin >> vec[i].first >> vec[i].second;
-    }
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
 }
 
-void solution(vector<pair<int, int>> vec) {
-
-    sort(vec.begin(), vec.end(), [](pair<int, int>& p1, pair<int, int>& p2) {
-        if (p1.first != p2.first) {
-            return p1.first < p2.first;
-        }
-        else {
-            return p1.second < p2.second;
-        }
-        });
-    vector<pair<int, int>> container;
-    container.push_back(vec[0]);
-    for (int i = 1; i < vec.size(); i++) {
-        auto p =  vec[i];
-        int start = container.back().first;
-        int end = container.back().second;
-
-        int curStart = p.first;
-        if (end <= curStart) {
-            container.push_back(p);
-        }
-        else {
-            int curEnd = p.second;
-            if (curEnd <= end) {
-                container.pop_back();
-                container.push_back(p);
-            }
-        }
-    }
-
-    cout << container.size() << endl;
-  
-    return;
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
 }
 
-int main()
-{
-    vector<pair<int, int>> vec;
+int g_nrow[] = { 0, 1, 0, -1 };
+int g_ncol[] = { 1, 0, -1, 0 };
 
-    input(vec);
-    solution(vec);
+struct Data {
+	int start;
+	int end;
+};
 
+bool cmp(const Data& d1, const Data& d2) {
+	return d1.start == d2.start ? d1.end < d2.end : d1.start < d2.start;
+}
+
+int GetMaxMeeting(vector<Data>& datas) {
+	sort(datas.begin(), datas.end(), cmp);
+	//for (int i = 0; i < datas.size(); i++) {
+	//	cout << datas[i].start << " " << datas[i].end << endl;
+	//}
+	int start = datas[0].start;
+	int end = datas[0].end;
+
+	int meeting = 1;
+
+	for (int i = 1; i < datas.size(); i++) {
+		int nextStart = datas[i].start;
+		int nextEnd = datas[i].end;
+
+		if (nextEnd < end) {
+			end = nextEnd;
+		}
+		else if (end <= nextStart) {
+			end = nextEnd;
+			start = nextStart;
+			meeting++;
+			//cout << start << " " << end << endl;
+		}
+
+		//cout << start << " " << end << endl;
+	}
+
+	return meeting;
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+
+	cin >> N;
+
+	vector<Data> datas;
+	for (int i = 0; i < N; i++) {
+		int s, e;
+		cin >> s >> e;
+		datas.push_back({ s, e });
+	}
+
+
+
+	cout << GetMaxMeeting(datas);
+
+
+	return 0;
 }
