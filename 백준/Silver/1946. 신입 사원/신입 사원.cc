23d364664ef227fcdede80f78e2c6f11d43d5ae1
jeontigger <boy9609@naver.com>
2024-10-26 13:34:47 +0900

    [Silver I] Title: 신입 사원, Time: 460 ms, Memory: 2924 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -1,57 +1,75 @@
-#pragma once
-#include <iostream>
 #include <vector>
-#include <list>
+#include <iostream>
+#include <string>
 #include <algorithm>
 #include <queue>
-#include <string>
+#include <cmath>
+#include <stack>
 
 using namespace std;
 
-void input(vector<vector<pair<int, int>>>& data) {
-    int n;
-    cin >> n;
+#define INF 1e9
 
-    data.resize(n);
-    for (size_t i = 0; i < n; i++)
-    {
-        int m;
-        cin >> m;
-        data[i].resize(m);
-        for (size_t j = 0; j < m; j++)
-        {
-            cin >> data[i][j].first >> data[i][j].second;
-        }
-    }
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
 }
 
-void solution(vector<vector<pair<int, int>>>& data) {
-
-    for (vector<pair<int, int>>& v : data) {
-        sort(v.begin(), v.end());
-
-        int minValue = v[0].second;
-
-        int cnt = 0;
-        for (auto& p : v) {
-            if (minValue < p.second) {
-                cnt++;
-            }
-            else {
-                minValue = p.second;
-            }
-        }
-        cout << v.size() - cnt << endl;
-        
-    }
-    
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
 }
 
-int main()
-{
-    vector<vector<pair<int, int>>> data;
+int g_nrow[] = { 0, 1, 0, -1 };
+int g_ncol[] = { 1, 0, -1, 0 };
 
-    input(data);
+struct Data {
+	int paper;
+	int meet;
+};
 
-    solution(data);
+bool cmp(const Data& d1, const Data& d2) {
+	return d1.paper < d2.paper;
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T, N;
+	cin >> T;
+
+	while (T) {
+		T--;
+		cin >> N;
+
+		vector<Data> juniors(N);
+		for (int i = 0; i < N; i++) {
+			cin >> juniors[i].paper >> juniors[i].meet;
+		}
+
+		sort(juniors.begin(), juniors.end(), cmp);
+
+		int cnt = 0;
+		int minValue = INF;
+		for (int i = 0; i < N; i++) {
+			if (minValue > juniors[i].meet) {
+				minValue = juniors[i].meet;
+				cnt++;
+			}
+		}
+
+		cout << cnt << " ";
+
+	}
+
+
+
+	return 0;
 }
