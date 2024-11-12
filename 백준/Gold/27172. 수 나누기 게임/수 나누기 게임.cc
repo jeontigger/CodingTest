5f1708760f528257c8bc1c0037d5707d7a7cb395
jeontigger <boy9609@naver.com>
2024-11-12 11:32:36 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 696 ms, Memory: 48920 KB -BaekjoonHub

---
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -0,0 +1,66 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	vector<int> nums(N);
+	unordered_map<int, bool> m;
+
+	int maxValue = 0;
+	for (int i = 0; i < N; i++) {
+		cin >> nums[i];
+		maxValue = max(nums[i], maxValue);
+		m[nums[i]] = true;
+	}
+
+	vector<int> score(1000001);
+
+	for (int i = 0; i < N; i++) {
+		int curNum = nums[i];
+
+		for (int j = curNum * 2; j < maxValue + 1; j += curNum) {
+			if (m[j]) {
+				score[curNum]++;
+				score[j]--;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << score[nums[i]] << " ";
+	}
+
+	return 0;
+}
