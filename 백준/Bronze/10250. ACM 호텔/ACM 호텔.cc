78630526e71a0b49ab66840d36edd09ac018295e
jeontigger <boy9609@naver.com>
2024-11-14 09:04:24 +0900

    [Bronze III] Title: ACM 호텔, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,69 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int HtoM(int H) {
+	return H * 60;
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+	int H, W, N;
+	while (T--) {
+		cin >> H >> W >> N;
+		int x = N % H;
+		int y = N / H;
+
+		if (x == 0) {
+			x = H;
+			y--;
+		}
+
+		int n = x * 100 + y + 1;
+		cout << n << endl;
+	}
+
+
+
+	return 0;
+}
+
+void printGrace() {
+	printf("	         ,r'\"7\n");
+	printf("r`-_   ,'  ,/\n");
+	printf("\\. \".L_r'");
+	printf("   `~\/");
+	printf("      |");
+	printf("      |");
+}
