8d951e2762c36402557df623b1dd66f0a77f8b2e
jeontigger <boy9609@naver.com>
2024-11-14 15:14:09 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -0,0 +1,49 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	string s;
+	cin >> N >> s;
+
+	int sum = 0;
+	for (int i = 0; i < s.size(); i++) {
+		sum += s[i] - '0';
+	}
+
+	cout << sum;
+
+	return 0;
+}
+
