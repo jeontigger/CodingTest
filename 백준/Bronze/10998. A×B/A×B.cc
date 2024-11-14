a197605534b5bd6d93b904dafc6e5b59c96a9614
jeontigger <boy9609@naver.com>
2024-11-14 15:05:52 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int A, B;
+	cin >> A >> B;
+	cout << A * B;
+
+	return 0;
+}
+
