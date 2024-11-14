fcbc374f8bb97e88d22bcbd01c55e6fed0413a61
jeontigger <boy9609@naver.com>
2024-11-14 14:53:34 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,48 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
