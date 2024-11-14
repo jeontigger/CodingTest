6b3abfac3203bdd84e80d2345567cf871b6c7e62
jeontigger <boy9609@naver.com>
2024-11-14 15:33:42 +0900

    [Bronze III] Title: 최소, 최대, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,51 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	int minValue = INF;
+	int maxValue = -INF;
+	int num;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		minValue = min(minValue, num);
+		maxValue = max(maxValue, num);
+	}
+
+	cout << minValue << " " << maxValue;
+
+	return 0;
+}
+
