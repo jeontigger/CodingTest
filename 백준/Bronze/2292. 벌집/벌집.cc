e9dc52ec7d1d1396f3e7681ab90af1c3f61d11d0
jeontigger <boy9609@naver.com>
2024-11-14 20:40:07 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,51 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	int num = 1;
+	int cnt = 1;
+	while (num < N) {
+		num += 6 * cnt;
+		cnt++;
+	}
+
+	cout << cnt;
+
+	return 0;
+}
+
