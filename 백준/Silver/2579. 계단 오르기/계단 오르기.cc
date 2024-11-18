4c6f82f4866142dcb931a6a8d5f44fe31a9838ff
jeontigger <boy9609@naver.com>
2024-11-19 08:59:28 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,69 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	vector<int> steps(N);
+	for (int i = 0; i < N; i++) {
+		cin >> steps[i];
+	}
+
+	if (N <= 1) {
+		cout << steps[0];
+		return 0;
+	}
+
+	vector<vector<int>> dp(N, vector<int>(2));
+	dp[0][0] = steps[0];
+	dp[0][1] = steps[0];
+	dp[1][0] = steps[0] + steps[1];
+	dp[1][1] = steps[1];
+
+
+	for (int i = 2; i < N; i++) {
+
+		// 한 칸 오르기
+		dp[i][0] = dp[i - 1][1] + steps[i];
+
+		// 두 칸 오르기
+		dp[i][1] = max(dp[i - 2][0], dp[i - 2][1]) + steps[i];
+	}
+
+	//PrintVec(dp);
+	cout << *max_element(dp.back().begin(), dp.back().end());
+
+	return 0;
+}
