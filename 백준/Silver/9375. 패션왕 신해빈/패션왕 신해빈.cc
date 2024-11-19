71c8fd8b0095729bcc756965df2edd82f6dc411c
jeontigger <boy9609@naver.com>
2024-11-20 08:58:32 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -37,22 +37,23 @@ int main() {
 	int T;
 	cin >> T;
 	while (T--) {
-		unordered_map<string, int> clotheCnts;
-		string name, kind;
 		int N;
 		cin >> N;
-		while (N--) {
-			cin >> name >> kind;
-			clotheCnts[kind]++;
+		unordered_map<string, int> wears;
+		string str;
+		for (int i = 0; i < N; i++) {
+			cin >> str >> str;
+			wears[str]++;
 		}
 
 		int res = 1;
-		for (auto iter = clotheCnts.begin(); iter != clotheCnts.end(); ++iter) {
-			res *= (iter->second + 1);
+		for (auto p : wears) {
+			res *= p.second + 1;
 		}
 
 		cout << res - 1 << ' ';
 	}
+
 
 	return 0;
 }
