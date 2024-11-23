4af6af3aee690a021229c99b70cf6622c6019e51
jeontigger <boy9609@naver.com>
2024-11-23 15:17:56 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 20 ms, Memory: 8288 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -0,0 +1,100 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+int N, M;
+vector<int> ladder;
+vector<int> snake;
+int SnakeLadder() {
+
+	queue<int> q;
+	q.push(1);
+
+	vector<int> table(101, INF);
+
+	int cnt = 0;
+	while (!q.empty()) {
+		cnt++;
+
+		int size = q.size();
+		while (size--) {
+			int curNum = q.front();
+			q.pop();
+			if (table[curNum] < cnt) continue;
+			table[curNum] = cnt;
+
+			for (int i = 1; i <= 6; i++) {
+				int nextNum = curNum + i;
+
+				if (nextNum > 100)continue;
+
+				if (nextNum == 100) {
+					return cnt;
+				}
+
+				if (ladder[nextNum] != 0) {
+					q.push(ladder[nextNum]);
+				}
+				else if (snake[nextNum] != 0) {
+					q.push(snake[nextNum]);
+				}
+				else {
+					q.push(nextNum);
+				}
+			}
+		}
+	}
+}
+
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	ladder.resize(101);
+	for (int i = 0; i < N; i++) {
+		int f, t;
+		cin >> f >> t;
+		ladder[f] = t;
+	}
+
+	snake.resize(101);
+	for (int i = 0; i < M; i++) {
+		int f, t;
+		cin >> f >> t;
+		snake[f] = t;
+	}
+
+	cout << SnakeLadder();
+
+	return 0;
+}
