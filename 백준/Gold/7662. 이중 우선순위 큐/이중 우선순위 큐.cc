78488e6c27b54df100e3101365e051c85bdb6b7d
jeontigger <boy9609@naver.com>
2024-11-23 16:30:32 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1220 ms, Memory: 57856 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,112 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
+void Insert(int n, priority_queue<int>& maxpq, priority_queue<int, vector<int>, greater<int>>& minpq, unordered_map<int, int>& m) {
+	maxpq.push(n);
+	minpq.push(n);
+	m[n]++;
+}
+
+void Delete(int type, priority_queue<int>& maxpq, priority_queue<int, vector<int>, greater<int>>& minpq, unordered_map<int, int>& m) {
+	// 1은 최대값 제거, -1은 최소값 제거
+	while (!minpq.empty() && m[minpq.top()] == 0) {
+		minpq.pop();
+	}
+	while (!maxpq.empty() && m[maxpq.top()] == 0) {
+		maxpq.pop();
+	}
+	if (type == 1) {
+
+		if (maxpq.empty()) return;
+		int curNum = maxpq.top();
+		m[curNum]--;
+		maxpq.pop();
+	}
+	else {
+		if (minpq.empty()) return;
+		int curNum = minpq.top();
+		m[curNum]--;
+		minpq.pop();
+	}
+	while (!maxpq.empty() && m[maxpq.top()] == 0) {
+		maxpq.pop();
+	}
+	while (!minpq.empty() && m[minpq.top()] == 0) {
+		minpq.pop();
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+	while (T--) {
+
+		int k;
+		cin >> k;
+
+		priority_queue<int> maxpq;
+		priority_queue<int, vector<int>, greater<int>> minpq;
+		unordered_map<int, int> m;
+		while (k--) {
+			char c;
+			int n;
+			cin >> c >> n;
+
+			if (c == 'I') {
+				Insert(n, maxpq, minpq, m);
+			}
+			else if (c == 'D') {
+				Delete(n, maxpq, minpq, m);
+			}
+			//if (maxpq.empty() || minpq.empty()) {
+			//	cout << "EMPTY\n";
+			//}
+			//else {
+			//	cout << maxpq.top() << ' ' << minpq.top() << '\n';
+			//}
+		}
+
+		if (maxpq.empty() || minpq.empty()) {
+			cout << "EMPTY\n";
+		}
+		else {
+			cout << maxpq.top() << ' ' << minpq.top() << '\n';
+		}
+
+
+	}
+
+	return 0;
+}
