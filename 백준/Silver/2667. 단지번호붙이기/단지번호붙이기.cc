b7f3a9eff0b85d5fbd6628b11709e6ac3c3c94ee
jeontigger <boy9609@naver.com>
2024-11-25 09:57:37 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -1,96 +1,85 @@
-#pragma once
-#include <iostream>
 #include <vector>
-#include <list>
+#include <iostream>
+#include <string>
 #include <algorithm>
 #include <queue>
-#include <string>
-#include <unordered_set>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
 
 using namespace std;
-int g_row;
-int g_col;
-int g_nrow[4] = { 0, 1, 0, -1 };
-int g_ncol[4] = { 1, 0, -1, 0 };
 
-int g_cnt = 0;
-int g_target = 0;
+#define INF 1e9
 
-void input(vector<vector<int>>& vec) {
-
-    int n;
-    cin >> n;
-
-    vec.resize(n);
-    for (size_t i = 0; i < n; i++)
-    {
-        string str;
-        cin >> str;
-        vec[i].resize(n);
-        for (size_t j = 0; j < str.length(); j++)
-        {
-            if (str[j] == '0') {
-                vec[i][j] = 0;
-            }
-            else {
-                vec[i][j] = 1;
-            }
-        }
-    }
-    
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << endl;
 }
 
-void solution(vector<vector<int>>& maps) {
-    
-    list<pair<int, int>> q;
-    vector<int> res;
-    for (size_t i = 0; i < maps.size(); i++)
-    {
-        for (size_t j = 0; j < maps[i].size(); j++)
-        {
-            if (maps[i][j] == 1) {
-                q.emplace_back(i, j);
-                maps[i][j] = 0;
-            }
-            int cnt = 0;
-            while (!q.empty()) {
-                auto pos = q.front();
-                q.pop_front();
-                cnt++;
-
-                for (size_t k = 0; k < 4; k++)
-                {
-                    int nrow = pos.first + g_nrow[k];
-                    int ncol = pos.second + g_ncol[k];
-
-                    if (0 <= nrow && nrow < maps.size() && 0 <= ncol && ncol < maps.size()) {
-                        if (maps[nrow][ncol] == 1) {
-                            q.emplace_back(nrow, ncol);
-                            maps[nrow][ncol] = 0;
-                        }
-                    }
-                }
-            }
-            if(cnt != 0)
-                res.push_back(cnt);
-        }
-    }
-    sort(res.begin(), res.end());
-    cout << res.size() << endl;
-    for (auto i : res) {
-        cout << i << " ";
-    }
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
 }
 
+int g_nrow[] = { 0, 1, 0, -1 };
+int g_ncol[] = { 1, 0, -1, 0 };
 
+int DFS(vector<vector<int>>& maps, int row, int col) {
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	int res = 1;
+	for (int i = 0; i < 4; i++) {
+		int nrow = row + g_nrow[i];
+		int ncol = col + g_ncol[i];
 
-    vector<vector<int>> maps;
-    input(maps);
-    solution(maps);
+		if (0 <= nrow && nrow < maps.size() && 0 <= ncol && ncol < maps.size()) {
+			if (maps[nrow][ncol] == 1) {
+				maps[nrow][ncol] = 0;
+				res += DFS(maps, nrow, ncol);
+			}
+		}
+	}
 
+	return res;
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	vector<vector<int>> maps(N, vector<int>(N));
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			char c;
+			cin >> c;
+			maps[i][j] = c - '0';
+		}
+	}
+
+	vector<int> res;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (maps[i][j] == 1) {
+				maps[i][j] = 0;
+				res.push_back(DFS(maps, i, j));
+			}
+		}
+	}
+
+	cout << res.size() << ' ';
+	sort(res.begin(), res.end());
+	PrintVec(res);
+
+	return 0;
 }
