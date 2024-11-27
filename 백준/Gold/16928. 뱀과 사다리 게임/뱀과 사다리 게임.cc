800e20adbf484fd943856e63158cfd2a62f8d434
jeontigger <boy9609@naver.com>
2024-11-27 16:22:50 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -18,7 +18,7 @@ void PrintVec(const vector<T>& v) {
 	for (T i : v) {
 		cout << i << " ";
 	}
-	cout << endl;
+	cout << '\n';
 }
 
 template<typename T>
@@ -28,17 +28,29 @@ void PrintVec(const vector<vector<T>>& vec) {
 	}
 }
 
-int N, M;
-vector<int> ladder;
-vector<int> snake;
-int SnakeLadder() {
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> portal(111);
+	for (int i = 0; i < N + M; i++) {
+		int v1, v2;
+		cin >> v1 >> v2;
+		portal[v1] = v2;
+	}
 
 	queue<int> q;
 	q.push(1);
 
-	vector<bool> visited(101);
+	vector<bool>visited(111);
+	visited[1] = true;
 
-	int cnt = 0;
+	int cnt = -1;
 	while (!q.empty()) {
 		cnt++;
 
@@ -46,55 +58,22 @@ int SnakeLadder() {
 		while (size--) {
 			int curNum = q.front();
 			q.pop();
-			if (visited[curNum]) continue;
-			visited[curNum] = true;
+
+			if (curNum == 100) {
+				cout << cnt;
+				return 0;
+			}
 
 			for (int i = 1; i <= 6; i++) {
-				int nextNum = curNum + i;
-
-				if (nextNum > 100)continue;
-
-				if (nextNum == 100) {
-					return cnt;
-				}
-
-				if (ladder[nextNum] != 0) {
-					q.push(ladder[nextNum]);
-				}
-				else if (snake[nextNum] != 0) {
-					q.push(snake[nextNum]);
-				}
-				else {
-					q.push(nextNum);
-				}
+				int next = curNum + i;
+				if (portal[next] != 0) next = portal[next];
+				if (visited[next]) continue;
+				visited[next] = true;
+				q.push(next);
 			}
 		}
 	}
-}
 
-
-int main() {
-
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
-	cin >> N >> M;
-	ladder.resize(101);
-	for (int i = 0; i < N; i++) {
-		int f, t;
-		cin >> f >> t;
-		ladder[f] = t;
-	}
-
-	snake.resize(101);
-	for (int i = 0; i < M; i++) {
-		int f, t;
-		cin >> f >> t;
-		snake[f] = t;
-	}
-
-	cout << SnakeLadder();
 
 	return 0;
 }
