3065b48dc9ec60985bb1a10e7b2b217924de54ca
jeontigger <boy9609@naver.com>
2024-12-03 11:04:16 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 40 ms, Memory: 5728 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -3,61 +3,66 @@
 #include <string>
 #include <algorithm>
 #include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
 
 using namespace std;
 
 #define INF 1e9
 
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+
 struct Edge {
-	int weight;
 	int to;
+	int w;
 };
 
 struct cmp {
 	bool operator()(const Edge& e1, const Edge& e2) {
-		return e1.weight > e2.weight;
+		return e1.w > e2.w;
 	}
 };
 
-void Input(vector<vector<Edge>>& edges, int& v1, int& v2) {
-	int V, E;
-	cin >> V >> E;
+int N, E;
+vector<vector<Edge>> edges;
 
-	edges.resize(V);
-	for (int i = 0; i < E; i++) {
-		int n1, n2, w;
-		cin >> n1 >> n2 >> w;
-		edges[n1 - 1].push_back({ w, n2 - 1 });
-		edges[n2 - 1].push_back({ w, n1 - 1 });
-	}
-
-	cin >> v1 >> v2;
-	v1--;
-	v2--;
-}
-
-int Dijkstra(const vector<vector<Edge>>& edges, int start, int end) {
-	priority_queue<Edge, vector<Edge>, cmp>pq;
-	vector<int> dist(edges.size(), INF);
-
+int Dijkstra(int start, int end) {
+	vector<int> dist(N + 1, INF);
 	dist[start] = 0;
-	pq.push({ 0, start });
+
+	priority_queue<Edge, vector<Edge>, cmp> pq;
+	pq.push({ start, 0 });
+
 
 	while (!pq.empty()) {
-		int curNode = pq.top().to;
-		int curWeight = pq.top().weight;
+		Edge edge = pq.top();
 		pq.pop();
 
-		if (dist[curNode] < curWeight) continue;
+		if (dist[edge.to] > edge.w) continue;
 
-		for (int i = 0; i < edges[curNode].size(); i++) {
-			int toNode = edges[curNode][i].to;
-			int toWeight = edges[curNode][i].weight;
-			int cost = curWeight + toWeight;
+		for (int i = 0; i < edges[edge.to].size(); i++) {
+			int next = edges[edge.to][i].to;
+			int cost = edges[edge.to][i].w + edge.w;
 
-			if (cost < dist[toNode]) {
-				dist[toNode] = cost;
-				pq.push({ cost, toNode });
+			if (dist[next] > cost) {
+				dist[next] = cost;
+				pq.push({ next, cost });
 			}
 		}
 	}
@@ -67,48 +72,38 @@ int Dijkstra(const vector<vector<Edge>>& edges, int start, int end) {
 
 int main() {
 
-	vector<vector<Edge>> edges;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> E;
+	edges.resize(N + 1);
+	for (int i = 0; i < E; i++) {
+		int v1, v2, c;
+		cin >> v1 >> v2 >> c;
+		edges[v1].push_back({ v2, c });
+		edges[v2].push_back({ v1, c });
+	}
+
 	int v1, v2;
+	cin >> v1 >> v2;
 
-	Input(edges, v1, v2);
+	int d1 = Dijkstra(1, v1);
+	int d2 = Dijkstra(1, v2);
 
-	int dist = 0, r1 = 0, r2 = 0;
+	int common = Dijkstra(v1, v2);
 
-	r1 += Dijkstra(edges, 0, v1);
-	if (r1 >= INF)
-	{
-		cout << -1 << endl;
-		return 0;
-	}
-	r2 += Dijkstra(edges, 0, v2);
+	int d1_2 = Dijkstra(v2, N);
+	int d2_1 = Dijkstra(v1, N);
 
-	if (r2 >= INF)
-	{
-		cout << -1 << endl;
-		return 0;
-	}
-	dist += Dijkstra(edges, v1, v2);
-	if (dist >= INF)
-	{
-		cout << -1 << endl;
+	if ((d1 == d2 && d1 == INF) || (d1_2 == d2_1 && d1_2 == INF) || common == INF) {
+		cout << -1;
 		return 0;
 	}
 
-	r2 += Dijkstra(edges, v1, edges.size() - 1);
-	if (r2 >= INF)
-	{
-		cout << -1 << endl;
-		return 0;
-	}
-	r1 += Dijkstra(edges, v2, edges.size() - 1);
-	if (r1 >= INF)
-	{
-		cout << -1 << endl;
-		return 0;
-	}
+	int res = common;
+	d1 + d1_2 > d2 + d2_1 ? res += d2 + d2_1 : res += d1 + d1_2;
+	cout << res;
 
-	dist += min(r1, r2);
-
-	cout << dist << endl;
 	return 0;
 }
