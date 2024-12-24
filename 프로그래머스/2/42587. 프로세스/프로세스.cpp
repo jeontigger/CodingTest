ae8c2a096f5e7eb2a3e6b16b18a8aedc75a6ec4e
jeontigger <boy9609@naver.com>
2024-12-24 10:57:26 +0900

    [level 2] Title: 프로세스, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -10,35 +10,30 @@ struct Data{
     int priority;
 };
 
-bool cmp(const Data& d1, const Data& d2){
-     return d1.priority != d2.priority ?d1.priority < d2.priority : d1.idx < d2.idx;
-}
-
 int solution(vector<int> priorities, int location) {
     int answer = 0;
     priority_queue<int> pq;
     queue<Data> q;
-    
-    int idx = 0;
-    for(int i:priorities){
-        pq.push(i);
-        q.push({idx++, i});
+    for(int i = 0 ; i < priorities.size(); i++){
+        pq.push(priorities[i]);
+        q.push({i, priorities[i]});
     }
     
     while(!q.empty()){
         Data data = q.front();
         q.pop();
+        
         if(data.priority == pq.top()){
             pq.pop();
             answer++;
             if(data.idx == location){
                 return answer;
             }
-            
+            continue;
         }else{
             q.push(data);
         }
     }
-    
+
     return answer;
 }
