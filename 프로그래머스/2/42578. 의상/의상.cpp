9357cbb671f0ca43b8da34eb7425ec54a6b5fa42
jeontigger <boy9609@naver.com>
2024-12-24 09:02:18 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,24 +1,21 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <unordered_map>
-#include <map>
 
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 0;
-    
-    unordered_map<string, int> m;
-    // 모든 부위 0 ~ n 경우의 수
-    for(int i = 0; i<clothes.size();i++){
-        m[clothes[i][1]]++;
+    unordered_map<string, int> um;
+    for(int i = 0 ; i < clothes.size(); i++){
+        um[clothes[i][1]]++;
     }
     
     answer = 1;
-    for(auto& p:m){
-        answer*=p.second + 1;
+    for(auto& p: um){
+        answer*= (p.second+1);
     }
     
-    return answer - 1;
+    
+    return answer-1;
 }
