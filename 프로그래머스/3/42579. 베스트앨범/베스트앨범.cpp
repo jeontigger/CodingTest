9f64e5165e22528366c75b927b0c03bc2f8fbfca
jeontigger <boy9609@naver.com>
2024-12-24 09:31:30 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,63 +1,68 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 struct Data{
-    int Play;
+    int val;
     int idx;
 };
 
-bool cmp(const Data& d1, const Data& d2){
-    return d1.Play != d2.Play ? d1.Play > d2.Play : d1.idx < d2.idx;
-}
-
-struct SumData{
-    string Name;
-    int sum;
+struct Data2{
+    string genre;
+    int val;
 };
 
-bool sumCmp(const SumData& d1, const SumData& d2){
-    return d1.sum > d2.sum;
+void PrintMap(unordered_map<string, vector<Data>>& um){
+    for(auto& p : um){
+        cout << p.first << ' ' ;
+        for(auto& d : p.second){
+            cout << d.val << ' ' ;
+        }
+        cout << '\n';
+    }
+}
+
+bool cmp(const Data&d1, const Data&d2){
+    return d1.val != d2.val ? d1.val > d2.val : d1.idx < d2.idx;
+}
+
+bool cmp2(const Data2&d1, const Data2&d2){
+    return d1.val > d2.val;
 }
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
-    map<string, vector<Data>> m;
     
-    vector<SumData> sums;
-    
-    for(int i = 0; i<genres.size(); i++){
-        const string& genre = genres[i];
-        m[genre].push_back({plays[i], i});
+    unordered_map<string, vector<Data>> um;
+    unordered_map<string, int> cntMap;
+    vector<Data2> v;
+    for(int i = 0 ; i < genres.size(); i++){
+        um[genres[i]].push_back({plays[i], i});
+        cntMap[genres[i]] += plays[i];
     }
     
-    for(auto p : m){
-        int sum = 0;
-        for(auto i : p.second){
-            sum += i.Play;
-        }
-        sums.push_back({p.first, sum});
+    for(auto& p: cntMap){
+        v.push_back({p.first, p.second});
     }
     
-    sort(sums.begin(), sums.end(), sumCmp);
+    sort(v.begin(), v.end(), cmp2);
     
-    for(auto& d: sums){
-        vector<Data>& v = m[d.Name];
-        sort(v.begin(), v.end(), cmp);
-        
-        int cnt = 0;
-        for(Data& i : v){
-            answer.push_back(i.idx);
-            cnt++;
-            if(cnt == 2)
-                break;
+    for(auto& p: um){
+        sort(p.second.begin(), p.second.end(), cmp);
+    }
+    
+    PrintMap(um);
+    
+    for(auto& d: v){
+        string genre = d.genre;
+        vector<Data>& cnts = um[genre];
+        for(int i = 0 ; i < 2 && i < cnts.size(); i++){
+            answer.push_back(cnts[i].idx);
         }
     }
-
     
     return answer;
 }
