295d9732d853d58c5a4e36f441149496e2a081a3
jeontigger <boy9609@naver.com>
2025-01-02 10:36:31 +0900

    [level 3] Title: 등굣길, Time: 0.06 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -4,70 +4,40 @@
 
 using namespace std;
 
-
-
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
-    vector<vector<int>> dp(n, vector<int>(m));
+    vector<vector<int>> maps(n, vector<int>(m));
     
-    // [j][i] = [j-1][i] + [j][i-1];
-    dp[0][0] = 1;
-    for(int i = 1 ; i < m; i++){
-        bool isPuddle = false;
-       for(int k = 0; k < puddles.size(); k++){
-           if(puddles[k][0]-1 == i && puddles[k][1] -1 == 0){
-               isPuddle = true;
-               break;
-           }
-       }
-
-       if(isPuddle) break;
-        dp[0][i] = dp[0][i-1];
+    for(int i = 0 ; i < puddles.size(); i++){
+        int row = puddles[i][1]-1;
+        int col = puddles[i][0]-1;
+        maps[row][col] = -1;
     }
     
-    for(int i = 1 ; i < n; i++){
-        bool isPuddle = false;
-       for(int k = 0; k < puddles.size(); k++){
-           if(puddles[k][0]-1 == 0 && puddles[k][1] -1 == i){
-               isPuddle = true;
-               break;
-           }
-       }
-
-       if(isPuddle) break;
-        dp[i][0] = dp[i-1][0];
+    maps[0][0] = 1;
+    for(int i = 0 ; i < n; i++){
+        for(int j = 0 ; j < m; j++){
+            if(i == 0 && j == 0) continue;
+            if(maps[i][j] == -1) continue;
+            
+            int top = 0;
+            int left = 0;
+            if(i > 0)
+                top = maps[i-1][j];
+            if(j > 0)
+                left = maps[i][j-1];
+            
+            if(top == -1) top = 0;
+            if(left == -1) left = 0;
+            maps[i][j] = (top + left) % 1'000'000'007;
+        }
     }
     
+    // for(auto v: maps){
+    //     for(int i : v){
+    //         cout << i << ' ';
+    //     }cout << '\n';
+    // }
     
-    
-    for(int i = 1 ; i < n ; i++){
-       for(int j = 1 ; j < m; j++){
-           
-           bool isPuddle = false;
-           for(int k = 0; k < puddles.size(); k++){
-               if(puddles[k][0]-1 == j && puddles[k][1] -1 == i){
-                   isPuddle = true;
-                   break;
-               }
-           }
-           
-           if(isPuddle) continue;
-           
-           dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007;
-           
-           
-           // for(vector<int>& v:dp){
-           //     for(int num : v){
-           //         cout << num << " " ;
-           //     }
-           //      cout << endl;
-           // }
-           // cout << endl;
-       }
-        
-    }
-    
-    answer = (dp[n-1][m-1]) % 1000000007;
-    
-    return answer;
+    return maps[n-1][m-1];
 }
