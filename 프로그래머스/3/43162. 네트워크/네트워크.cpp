ca1f530a76d2c9fa698416233af20b953e4f2d0b
jeontigger <boy9609@naver.com>
2025-01-06 09:24:12 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,43 +1,48 @@
 #include <string>
 #include <vector>
 #include <queue>
-
-struct Pos{
-    int row;
-    int col;
-};
-
-int g_nrow[] = {0, 1, 0, -1};
-int g_ncol[] = {1, 0, -1, 0};
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
-    vector<vector<int>> maps(computers);
-    vector<bool> visited(n);
-    queue<int> q;
+vector<vector<int>> g_computers;
+vector<bool> visited;
 
-    for(int i = 0 ; i < n ; i++){
-        if(visited[i]) continue;
+void NearComputer(int start){
+    visited[start] = true;
+    queue<int> q;
+    q.push(start);
+    
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
         
-        q.push(i);
-        visited[i] = true;
+        // cout << cur << ' ';
         
-        answer++;
-        
-        while(!q.empty()){
-            int idx = q.front();
-            q.pop();
-            
-            for(int j = 0 ; j < n; j++){
-                if(computers[idx][j] == 1 && !visited[j]){
-                    q.push(j);
-                    visited[j] = true;
-                }
+        for(int i = 0 ; i < g_computers[cur].size(); i++){
+            int next = i;
+            if(!visited[next] && g_computers[cur][next]){
+                visited[next] = true;
+                q.push(next);
             }
         }
     }
+}
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    visited.resize(n);
+    g_computers = computers;
+    
+    for(int i = 0 ;i <n; i++){
+        if(!visited[i]){
+            NearComputer(i);
+            // cout << i << ' ';
+            // cout << '\n';
+            answer++;
+        }
+    }
+    
     
     return answer;
 }
