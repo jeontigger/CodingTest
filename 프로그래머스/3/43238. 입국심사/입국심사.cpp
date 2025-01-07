f0023aa4db2706d8091e6edd0fdb1fcae1d64ba4
jeontigger <boy9609@naver.com>
2025-01-07 09:27:04 +0900

    [level 3] Title: 입국심사, Time: 34.50 ms, Memory: 6.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,37 +1,37 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-bool IsCover(const vector<int>& times, long long curll, int n){
-    long long coverCnt = 0;
-    
-    for(int i = 0 ; i < times.size();i++){
-        coverCnt += (curll / times[i]);
+bool IsValid(long long time, long long n, const vector<int>& times){
+    long long acc = 0;
+    for(int i = 0 ; i < times.size(); i++){
+        acc += time/times[i];
+        
+        if(acc >= n) return true;
     }
     
-    return coverCnt >= n ? true : false;
+    return false;
 }
 
 long long solution(int n, vector<int> times) {
     long long answer = 0;
-    long long right = *max_element(times.begin(), times.end());
-    right *= n;
+    
     long long left = 0;
+    long long right = 1e15;
     
-    
-    while(left < right){
-        long long mid = (left + right) / 2;
+    while(left <=right){
+        long long mid = (left+right)/2;
         
-        if(IsCover(times, mid, n)){
-            right = mid;
-            answer = right;
+        if(IsValid(mid, n, times)){
+            right = mid - 1;
         }else{
-            left = mid+1;
+            left = mid + 1;
         }
     }
     
+    // cout << left << ' ' << right;
+    answer = left;
     return answer;
 }
