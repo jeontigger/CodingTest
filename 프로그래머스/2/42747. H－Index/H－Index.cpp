696e9796c74ba57ec20fc1a4a4e1e69edae937b5
jeontigger <boy9609@naver.com>
2025-08-22 23:41:14 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -9,11 +9,10 @@ int solution(vector<int> citations) {
     int answer = 0;
     sort(citations.begin(), citations.end());
     
-    for(int i = 0; i < citations.size(); i++){
-        int curCnt = citations.size()-i;
+    for(int i = 0 ; i < citations.size(); i++){
+        int curCnt = citations.size()- i;
         if(citations[i] >= curCnt)
             return curCnt;
     }
-    
     return answer;
 }
