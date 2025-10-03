5dc1af63d2a52682f3b6176d407db95e9346d428
jeontigger <boy9609@naver.com>
2025-10-04 01:21:23 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 836 ms, Memory: 212040 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cc
@@ -0,0 +1,68 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+
+set<string> s;
+string str;
+void Inputs() {
+	cin >> str;
+}
+
+void Solution() {
+	for (int i = 1; i <= str.size(); i++) {
+		for (int j = 0; j < str.size() + 1 - i; j++) {
+			s.insert(str.substr(j, i));
+		}
+	}
+
+	cout << s.size();
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		Inputs();
+		Solution();
+	}
+
+
+	return 0;
+}
