47a424165ba4aab55929323b14d5acab2dd62500
jeontigger <boy9609@naver.com>
2025-10-03 20:00:19 +0900

    [Bronze I] Title: 소인수분해, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,77 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+
+int N;
+
+void Inputs() {
+	cin >> N;
+}
+
+void Solution() {
+	while (N != 1) {
+
+		if (N % 2 == 0) {
+			N /= 2;
+			cout << 2 << '\n';
+		}
+		else {
+			for (int i = 3; i <= N; i += 2) {
+				if (N % i == 0) {
+					cout << i << '\n';
+					N /= i;
+					break;
+				}
+			}
+		}
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		Inputs();
+		Solution();
+	}
+
+
+	return 0;
+}
