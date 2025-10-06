3ed4cdbccc1d6d94066b906037e79e6a149e1e4c
jeontigger <boy9609@naver.com>
2025-10-07 01:14:12 +0900

    [Silver IV] Title: 큐 2, Time: 324 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,106 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+
+int N;
+
+void Inputs() {
+
+}
+
+void Solution() {
+	cin >> N;
+
+	queue<int> q;
+	FOR(i, N) {
+		string comm;
+		cin >> comm;
+
+		if (comm == "push") {
+			int num;
+			cin >> num;
+
+			q.push(num);
+		}
+		else if (comm == "pop") {
+			if (q.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (comm == "size") {
+			cout << q.size() << '\n';
+		}
+		else if (comm == "empty") {
+			cout << q.empty() << '\n';
+		}
+		else if (comm == "front") {
+			if (q.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << q.front() << '\n';
+			}
+		}
+		else if (comm == "back") {
+			if (q.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << q.back() << '\n';
+			}
+		}
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		Inputs();
+		Solution();
+	}
+
+
+	return 0;
+}
