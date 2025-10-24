26be1d996fddd4788f8cdd68647f6406baf13b4b
jeontigger <boy9609@naver.com>
2025-10-24 18:38:44 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,73 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+
+int N, minValue, maxValue;
+
+void Inputs() {
+	cin >> N;
+	minValue = INF;
+	maxValue = 0;
+
+	FOR(i, N) {
+		int num;
+		cin >> num;
+
+		minValue = min(minValue, num);
+		maxValue = max(maxValue, num);
+	}
+}
+
+void Solution() {
+	cout << minValue * maxValue;
+
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		Inputs();
+		Solution();
+	}
+
+
+	return 0;
+}
