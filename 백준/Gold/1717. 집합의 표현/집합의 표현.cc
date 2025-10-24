87181b5c037de8370361f289816c62ea05085a77
jeontigger <boy9609@naver.com>
2025-10-24 19:28:54 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 6324 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -5,17 +5,24 @@
 #include <queue>
 #include <cmath>
 #include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
 
 using namespace std;
 
-//#define INF 1e9
+#define INF 1e9
 
 template<typename T>
 void PrintVec(const vector<T>& v) {
 	for (T i : v) {
 		cout << i << " ";
 	}
-	cout << endl;
+	cout << '\n';
 }
 
 template<typename T>
@@ -24,15 +31,17 @@ void PrintVec(const vector<vector<T>>& vec) {
 		PrintVec(v);
 	}
 }
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
 
-vector<int> root;
+int N, M;
+vector<int> roots;
 
 int Find(int n) {
-	if (root[n] == n) {
+	if (roots[n] == n) {
 		return n;
 	}
 	else {
-		return root[n] = Find(root[n]);
+		return roots[n] = Find(roots[n]);
 	}
 }
 
@@ -42,34 +51,52 @@ void Union(int a, int b) {
 
 	if (a == b) return;
 
-	root[max(a, b)] = min(a, b);
+	roots[max(a, b)] = min(a, b);
+}
+
+void Inputs() {
+	cin >> N >> M;
+
+	roots.resize(N + 1);
+	FOR(i, N + 1) {
+		roots[i] = i;
+	}
+
+	FOR(i, M) {
+		int c, a, b;
+		cin >> c >> a >> b;
+		if (c == 0) {
+			Union(a, b);
+		}
+		else {
+			if (Find(a) == Find(b)) {
+				cout << "YES\n";
+			}
+			else {
+				cout << "NO\n";
+			}
+		}
+	}
+
+}
+
+
+void Solution() {
+
 }
 
 int main() {
 
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+	cout.tie(NULL);
 
-	int n, m;
-	cin >> n >> m;
-	root.resize(n + 1);
-	for (int i = 0; i < root.size(); i++) {
-		root[i] = i;
-	}
-	int c, v1, v2;
-	for (int i = 0; i < m; i++) {
-		cin >> c >> v1 >> v2;
-		if (c == 0) {
-			Union(v1, v2);
-		}
-		else {
-			if (Find(v1) == Find(v2)) {
-				printf("YES\n");
-			}
-			else {
-				printf("NO\n");
-			}
-		}
+
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		Inputs();
+		Solution();
 	}
 
 
