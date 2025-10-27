f70a65e177aad322f9f7145a2be052d628601d35
jeontigger <boy9609@naver.com>
2025-10-28 02:58:56 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 224 ms, Memory: 6124 KB -BaekjoonHub

---
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.cc
@@ -0,0 +1,73 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+
+int N, K;
+vector<int>score;
+
+void Inputs() {
+	cin >> N;
+	score.resize(N);
+	FOR(i, N)
+		cin >> score[i];
+	cin >> K;
+}
+
+void Solution() {
+
+	int k = N / K;
+	for (int i = 0; i < N; i += k) {
+		sort(score.begin() + i, score.begin() + i + k);
+	}
+
+	PrintVec(score);
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		Inputs();
+		Solution();
+	}
+
+
+	return 0;
+}
