c3de6f4f5b1976716a2aa14f197578bc5e702f95
jeontigger <boy9609@naver.com>
2025-11-05 02:25:02 +0900

    [Gold IV] Title: 동전 1, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,72 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+
+int n, k;
+int val[101];
+
+void Inputs() {
+	cin >> n >> k;
+	FOR(i, n)
+		cin >> val[i];
+}
+
+void Solution() {
+	vector<int> dp(10001);
+	for (int i = 0; i < n; i++) {
+		dp[val[i]]++;
+		for (int j = val[i]; j <= k; j++) {
+			dp[j] += dp[j - val[i]];
+		}
+	}
+	cout << dp[k] << '\n';
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		Inputs();
+		Solution();
+	}
+
+
+	return 0;
+}
