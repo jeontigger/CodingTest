9aa5bcb3858544835cdffbe388c5e58d53b2a564
jeontigger <boy9609@naver.com>
2025-11-20 00:27:37 +0900

    [Silver IV] Title: blobyum, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/24499. blobyum/blobyum.cc
@@ -0,0 +1,76 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
+
+using namespace std;
+
+#define INF 1e9
+
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
+
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+
+int N, K, A[100000];
+
+void Inputs() {
+	cin >> N >> K;
+	FOR(i, N)
+		cin >> A[i];
+}
+
+void Solution() {
+	int left = 0, sum = 0, ret = 0;
+	for (int right = 0; right < N + K; right++) {
+		sum += A[right % N];
+
+		while (right - left >= K) {
+			sum -= A[left % N];
+			left++;
+		}
+
+		ret = max(ret, sum);
+	}
+
+	cout << ret << '\n';
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		Inputs();
+		Solution();
+	}
+
+
+	return 0;
+}
