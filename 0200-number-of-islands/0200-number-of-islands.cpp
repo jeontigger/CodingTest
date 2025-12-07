64b85cc343e4fdbd6542199836e4522de2919c1e
jeontigger <boy9609@naver.com>
2025-12-08 02:24:44 +0900

    Time: 25 ms (71.61%), Space: 16.7 MB (58.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,88 +1,44 @@
 class Solution {
 public:
-    void init(vector<bool>& confirm, const vector<vector<char>>& grid){
-        confirm.resize(grid.size()*grid[0].size());
-
-        for(int i = 0; i< confirm.size();i++){
-            int row = i / grid[0].size();
-            int col = i % grid[0].size();
-            if(grid[row][col] == '0'){
-                confirm[i] = true;
-            }
-        }        
+    int dr[4] = {0, 1, 0, -1};
+    int dc[4] = {1, 0, -1, 0};
+    int N, M;
+    vector<vector<char>> m_grid;
+    bool IsValid(int r, int c){
+        if(r < 0 || r >= N || c < 0 || c >= M)return false;
+        return m_grid[r][c] == '1';
     }
 
-    void printConfirm(vector<bool>& confirm, int size){
+    int DFS(int r, int c) {
+        int ret = 1;
 
-        int cnt = 0;
-        for(bool b: confirm){
-            cout << b << " ";
-            cnt++;
-            if(size <= cnt){
-                cout <<endl;
-                cnt = 0;
+        for(int i = 0 ; i < 4; i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(IsValid(nr, nc)){
+                m_grid[nr][nc] = '0';
+                ret += DFS(nr, nc);
             }
         }
+
+        return ret;
     }
-
-    // bfs로 풀기
     int numIslands(vector<vector<char>>& grid) {
-        queue<int> task;
-        
-        vector<bool> confirm;
+        int ret = 0;
+        m_grid = grid;
+        N = grid.size(), M = grid[0].size();
 
-        init(confirm, grid);
-        int arr[4];
-        arr[0] = -1;
-        arr[1] = 1;
-        arr[2] = -grid[0].size();
-        arr[3] = grid[0].size();
-
-        int res = 0;
-        printConfirm(confirm, grid[0].size());
-        for(int i = 0 ; i<confirm.size();i++){
-            if(confirm[i] == false){
-                task.push(i);
-                res++;
-                cout << endl;
-                // printConfirm(confirm, grid[0].size());
-                // cout << "=========================== " << res-1 << endl;
-            }
-
-            while(!task.empty()){
-                int pos = task.front();
-                task.pop();
-
-                confirm[pos] = true;
-                cout << pos << " ";
-
-                int row = pos / grid[0].size();
-                int col = pos % grid[0].size();
-
-
-                // 4방향 검사 - pos - 1,  pos + 1, pos + col , pos - col
-                for(int j =0; j<4; j++){
-                    if(pos % grid[0].size() == 0 && j == 0){
-                        continue;
-                    }else if(pos % grid[0].size()== grid[0].size()-1 && j == 1){
-                        continue;
-                    }
-                    int dir = pos + arr[j];
-                    if(0<=dir && dir <confirm.size()){
-                        if(!confirm[dir]){
-                            confirm[dir] = true;
-                            task.push(dir);
-                        }
-                    }
+        for(int i = 0 ; i < N; i++){
+            for(int j = 0 ; j < M; j++){
+                if(IsValid(i, j)){
+                    m_grid[i][j] = '0';
+                    DFS(i, j);
+                    ret++;
                 }
             }
-
         }
-        // cout << endl;
-        // printConfirm(confirm, grid[0].size());
-        // cout << "=========================== " << res << endl;
 
-        return res;
-
+        return ret;
     }
 };
