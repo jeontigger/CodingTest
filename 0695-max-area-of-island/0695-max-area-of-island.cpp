820f8e606cd13c409ca82e9ac4ce889bc84c3d19
jeontigger <boy9609@naver.com>
2025-12-08 02:25:07 +0900

    Time: 6 ms (29.66%), Space: 28.5 MB (44.6%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int dr[4] = {0, 1, 0, -1};
+    int dc[4] = {1, 0, -1, 0};
+    int N, M;
+    vector<vector<int>> m_grid;
+    bool IsValid(int r, int c){
+        if(r < 0 || r >= N || c < 0 || c >= M)return false;
+        return m_grid[r][c] == 1;
+    }
+
+    int DFS(int r, int c) {
+        int ret = 1;
+
+        for(int i = 0 ; i < 4; i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(IsValid(nr, nc)){
+                m_grid[nr][nc] = 0;
+                ret += DFS(nr, nc);
+            }
+        }
+
+        return ret;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int ret = 0;
+        m_grid = grid;
+        N = grid.size(), M = grid[0].size();
+
+        for(int i = 0 ; i < N; i++){
+            for(int j = 0 ; j < M; j++){
+                if(IsValid(i, j)){
+                    m_grid[i][j] = 0;
+                    ret = max(ret, DFS(i, j));
+                }
+            }
+        }
+
+        return ret;
+    }
+};
