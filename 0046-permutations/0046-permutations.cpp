bb3c22a0906ddc79ac87785ab1f6440d82f87dba
jeontigger <boy9609@naver.com>
2025-12-08 23:16:07 +0900

    Time: 2 ms (37.48%), Space: 10.6 MB (48.84%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-    vector<bool> confirm;
-    vector<vector<int>> res;
-    void permutation(vector<int> product,int cnt, const vector<int>& nums){
-        if(cnt == nums.size()){
-            res.push_back(product);
-            return;
-        }
-
-        int i;
-        for(i = 0; i<nums.size();i++){
-            if(!confirm[nums[i]+10]){
-                confirm[nums[i]+10] = true;
-                product.push_back(nums[i]);
-                permutation(product, cnt+1, nums);
-                product.pop_back();
-                confirm[nums[i]+10] = false;
-            }
-            
-        }
-
-        
-    }
     vector<vector<int>> permute(vector<int>& nums) {
-        confirm.resize(21);
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ret;
 
-        permutation({}, 0, nums);
-
-        return res;
-
+        do{
+            ret.push_back(nums);
+        }while(next_permutation(nums.begin(), nums.end()));
+        
+        return ret;
     }
 };
