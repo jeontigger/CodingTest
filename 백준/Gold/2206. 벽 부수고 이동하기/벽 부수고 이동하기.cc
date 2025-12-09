1bab5b8a257bf4508eb6ff85e09f28643cce196e
jeontigger <boy9609@naver.com>
2025-12-10 02:30:49 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 64 ms, Memory: 12280 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,160 +1,120 @@
-#pragma once
-#include <iostream>
 #include <vector>
-#include <list>
+#include <iostream>
+#include <string>
 #include <algorithm>
 #include <queue>
-#include <string>
-#include <unordered_set>
-#include <limits.h>
+#include <cmath>
+#include <stack>
+#include <unordered_map>
+#include <set>
+#include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
 
 using namespace std;
-int g_row;
-int g_col;
-int g_nrow[4] = { 0, 1, 0, -1 };
-int g_ncol[4] = { 1, 0, -1, 0 };
 
-int g_cnt = 0;
-int g_target = 0;
-vector<int> g_dp;
+#define INF 1e9
 
-#pragma region FUNC
+template<typename T>
+void PrintVec(const vector<T>& v) {
+	for (T i : v) {
+		cout << i << " ";
+	}
+	cout << '\n';
+}
 
-struct fData {
-    int row;
-    int col;
-    int cnt;
-    bool b;
-    fData(int _cnt, int _row, int _col, bool _b) { cnt = _cnt; row = _row; col = _col; b = _b; }
-    friend ostream& operator <<(ostream& os, const fData& data);
+template<typename T>
+void PrintVec(const vector<vector<T>>& vec) {
+	for (auto& v : vec) {
+		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+int dr[4] = { 0, 1, 0, -1 };
+int dc[4] = { 1, 0, -1, 0 };
+
+int N, M;
+vector<vector<int>> maps;
+vector<vector<int>> visited(N, vector<int>(M, 2));
+bool Inputs() {
+	cin >> N >> M;
+	maps.resize(N, vector<int>(M));
+	visited.resize(N, vector<int>(M, 2));
+	FOR(r, N)
+		FOR(c, M) {
+		char ch;
+		cin >> ch;
+		maps[r][c] = ch - '0';
+	}
+
+
+	return true;
+}
+
+struct Pos {
+	int r;
+	int c;
+	int b;
+	int s;
 };
 
-ostream& operator<<(ostream& os, fData data)
-{
-    os << data.row << " " << data.col << endl;
-    return os;
+bool IsValid(int r, int c) {
+	if (r < 0 || r >= N || c < 0 || c >= M)return false;
+	return true;
 }
 
+void Solution() {
 
-template<typename T>
-void printVec(vector<T>& vec) {
-    for (size_t i = 0; i < vec.size(); i++)
-    {
-        cout << vec[i] << " ";
-    }
-    cout << endl;
+	queue<Pos>q;
+	q.push({ 0, 0, 0, 0 });
+	visited[0][0] = 0;
+
+	while (!q.empty()) {
+		Pos pos = q.front();
+		q.pop();
+
+		//cout << pos.r << ' ' << pos.c << ' ' << pos.b << ' ' << pos.s << '\n';
+
+		if (pos.r == N - 1 && pos.c == M - 1) {
+			cout << pos.s + 1 << '\n';
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nr = pos.r + dr[i];
+			int nc = pos.c + dc[i];
+
+			if (IsValid(nr, nc)) {
+				int nb = pos.b;
+				if (maps[nr][nc] == 1) {
+					nb++;
+				}
+
+				if (visited[nr][nc] > nb) {
+					visited[nr][nc] = nb;
+					q.push({ nr, nc, nb, pos.s + 1 });
+				}
+			}
+		}
+	}
+
+	cout << -1 << '\n';
 }
 
-template <typename T>
-void printDoubleVec(vector<vector<T>>& vec) {
-    for (size_t i = 0; i < vec.size(); i++)
-    {
-        printVec(vec[i]);
-    }
-}
+int main() {
 
-template<typename T>
-void inputVec(vector<T>& vec, int n) {
-    vec.resize(n);
-    for (size_t i = 0; i < vec.size(); i++)
-    {
-        cin >> vec[i];
-    }
-}
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-template<typename T>
-void inputDoubleVec(vector<vector<T>>& vec, int r, int c) {
-    vec.resize(r);
-    for (size_t i = 0; i < r; i++)
-    {
-        inputVec(vec[i], c);
-    }
-}
-#pragma endregion
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		if (!Inputs()) break;
+		Solution();
+	}
 
-void input(vector<vector<int>>& graph) {
-    int row, col;
-    cin >> row >> col;
-    graph.resize(row);
-    for (size_t i = 0; i < row; i++)
-    {
-        graph[i].resize(col);
-        string str;
-        cin >> str;
-        for (size_t j = 0; j < col; j++)
-        {
-            graph[i][j] = str[j] - '0';
-        }
-    }
-}
-int minValue;
-
-void bfs(vector<vector<int>>& graph) {
-    list<fData> q;
-    q.emplace_back(1, 0, 0, false);
-    vector<vector<int>> visited(graph.size(), vector<int>(graph[0].size(), 0));
-    vector<vector<int>> visited_b(graph.size(), vector<int>(graph[0].size(), 0));
-    visited[0][0] = true;
-
-    //printDoubleVec(visited);
-
-    while (!q.empty()) {
-        auto data = q.front();
-        q.pop_front();
-        //cout << data << endl;
-
-        if (data.row == graph.size() - 1 && data.col == graph[0].size() - 1) {
-            minValue = min(minValue, data.cnt);
-            
-            break;
-        }
-
-        for (int i = 0; i < 4; i++) {
-            int nrow = data.row + g_nrow[i];
-            int ncol = data.col + g_ncol[i];
-
-            if (0 <= nrow && nrow < graph.size() && 0 <= ncol && ncol < graph[0].size()) {
-                if (data.b) {
-                    if (!visited_b[nrow][ncol] && graph[nrow][ncol] == 0) {
-                        q.emplace_back(data.cnt + 1, nrow, ncol, true);
-                        visited_b[nrow][ncol] = true;
-                    }
-                }
-                else {
-                    if (!visited[nrow][ncol] && graph[nrow][ncol] == 0) {
-                        q.emplace_back(data.cnt + 1, nrow, ncol, false);
-                        visited[nrow][ncol] = true;
-                    }
-                    else if (graph[nrow][ncol] == 1) {
-                        q.emplace_back(data.cnt + 1, nrow, ncol, true);
-                        visited_b[nrow][ncol] = true;
-                    }
-                }
-            }
-        }
-    }
-}
-
-void solution(vector<vector<int>>& graph) {
-    minValue = INT_MAX;
-    bfs(graph);
-
-    if (minValue == INT_MAX) {
-        cout << -1 << endl;
-    }
-    else {
-        cout << minValue << endl;
-    }
-}
-
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    vector<vector<int>> graph;
-    input(graph);
-    solution(graph);
-
+	return 0;
 }
