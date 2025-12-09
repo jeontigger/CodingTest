b5494782295b3bfa3bdf89186301b3b8c0196afc
jeontigger <boy9609@naver.com>
2025-12-10 02:48:57 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
@@ -8,6 +8,10 @@
 #include <unordered_map>
 #include <set>
 #include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
 
 using namespace std;
 
@@ -18,7 +22,7 @@ void PrintVec(const vector<T>& v) {
 	for (T i : v) {
 		cout << i << " ";
 	}
-	cout << endl;
+	cout << '\n';
 }
 
 template<typename T>
@@ -27,45 +31,88 @@ void PrintVec(const vector<vector<T>>& vec) {
 		PrintVec(v);
 	}
 }
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+int dr[4] = { 0, 1, 0, -1 };
+int dc[4] = { 1, 0, -1, 0 };
 
+int N, M;
+vector<vector<int>> edges;
+
+bool Inputs() {
+	cin >> N >> M;
+	edges.resize(N + 1);
+	FOR(i, M) {
+		int n1, n2;
+		cin >> n1 >> n2;
+		edges[n1].push_back(n2);
+		edges[n2].push_back(n1);
+	}
+
+	//for (int i = 0; i < edges.size(); i++) {
+	//	sort(edges[i].begin(), edges[i].end());
+	//	edges[i].erase(unique(edges[i].begin(), edges[i].end()), edges[i].end());
+	//}
+	return true;
+}
+
+struct Data {
+	int node;
+	int dist;
+};
+
+int KB(int n) {
+	vector<int> distance(N + 1, INF);
+	distance[n] = 0;
+
+	queue<Data> q;
+	q.push({ n, 0 });
+
+	while (!q.empty()) {
+		Data cur = q.front();
+		q.pop();
+
+		for (int i = 0; i < edges[cur.node].size(); i++) {
+			int next = edges[cur.node][i];
+			if (distance[next] > cur.dist + 1) {
+				distance[next] = cur.dist + 1;
+				q.push({ next, cur.dist + 1 });
+			}
+		}
+	}
+
+	int ret = 0;
+	for (int i = 1; i <= N; i++) {
+		ret += distance[i];
+	}
+
+	return ret;
+}
+
+void Solution() {
+
+	int minValue = INF;
+	int res = 0;
+	for (int i = 1; i <= N; i++) {
+		int ret = KB(i);
+		if (ret < minValue) {
+			minValue = ret;
+			res = i;
+		}
+	}
+	cout << res << '\n';
+}
 int main() {
 
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
-
-	vector<vector<int>> graph(N + 1, vector<int>(N + 1, INF));
-	for (int i = 0; i < M; i++) {
-		int v1, v2;
-		cin >> v1 >> v2;
-		graph[v1][v2] = 1;
-		graph[v2][v1] = 1;
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		if (!Inputs()) break;
+		Solution();
 	}
 
-	for (int k = 1; k < N + 1; k++) {
-		for (int i = 1; i < N + 1; i++) {
-			for (int j = 1; j < N + 1; j++) {
-				graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
-			}
-		}
-	}
-
-	int minValue = INF;
-	int res = -1;
-	for (int i = 1; i < N + 1; i++) {
-		int sum = 0;
-		for (int j = 1; j < N + 1; j++) {
-			sum += graph[i][j];
-		}
-		if (minValue > sum) {
-			minValue = sum;
-			res = i;
-		}
-	}
-
-	cout << res;
 	return 0;
 }
