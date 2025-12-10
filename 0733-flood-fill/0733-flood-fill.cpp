d33ca5aafb70e6526269d9370b904f8421239ff2
jeontigger <boy9609@naver.com>
2025-12-11 02:17:43 +0900

    Time: 4 ms (6.05%), Space: 18.7 MB (8.91%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    int N, M, m_color;
-    vector<vector<int>> m_image;
-
-    bool IsValid(int r, int c, int color){
-        if(r < 0 || r >= N || c < 0 || c >= M)return false;
-        return m_image[r][c] == color;
+    bool IsValid(int r, int c, int origin, vector<vector<int>>& image,
+                 vector<vector<int>>& visited) {
+        return !(r < 0 || r >= visited.size() || c < 0 ||
+                 c >= visited[0].size()) &&
+               !visited[r][c] && image[r][c] == origin;
     }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
+        queue<pair<int, int>> q;
+        int dr[4] = {0, 1, 0, -1};
+        int dc[4] = {1, 0, -1, 0};
+        int origin = image[sr][sc];
 
-    int dr[4] = {0, 1, 0, -1};
-    int dc[4] = {1, 0, -1, 0};
+        vector<vector<int>> visited(image.size(), vector<int>(image[0].size()));
+        q.push({sr, sc});
+        visited[sr][sc] = true;
+        image[sr][sc] = color;
 
-    void DFS(int r, int c, int color){
-        cout << r << ' ' << c << '\n';
-        for(int i = 0 ; i < 4; i++){
-            int nr = r + dr[i];
-            int nc = c + dc[i];
+        while (!q.empty()) {
+            auto pos = q.front();
+            q.pop();
 
-            if(IsValid(nr, nc, color)){
-                m_image[nr][nc] = m_color;
-                DFS(nr, nc, color);
+            for (int i = 0; i < 4; i++) {
+                int nr = pos.first + dr[i];
+                int nc = pos.second + dc[i];
+                if (IsValid(nr, nc, origin, image, visited)) {
+                    // cout << pos.first << ' ' << pos.second << ' ' << nr << ' ' << nc << '\n';
+                    visited[nr][nc] = true;
+                    image[nr][nc] = color;
+                    q.push({nr, nc});
+                }
             }
         }
-    }
-
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        m_image = image;
-        m_color = color;
-
-        N = m_image.size(), M = m_image[0].size();
-
-        m_image[sr][sc] = m_color;
-        if(image[sr][sc] == color)return image;
-        DFS(sr, sc, image[sr][sc]);
-
-        return m_image;
+        return image;
     }
 };
