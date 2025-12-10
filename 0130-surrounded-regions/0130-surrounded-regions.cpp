14a0eeeda8d11aab7a8c1ee442f65aeb39c75e72
jeontigger <boy9609@naver.com>
2025-12-11 02:24:31 +0900

    Time: 0 ms (100%), Space: 15.4 MB (17.58%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,54 +1,61 @@
 class Solution {
 public:
-    int N, M;
-    vector<vector<int>> visited;
-    vector<vector<char>> m_board;
-    vector<vector<char>> g_board;
-    int dr[4] = {0, 1, 0, -1};
-    int dc[4] = {1, 0, -1, 0};
-
-    bool IsValid(int r, int c) {
-        if (r < 0 || r >= N || c < 0 || c >= M)
-            return false;
-        return !visited[r][c] && g_board[r][c] == 'O';
+int N, M;
+vector<vector<int>>visited;
+vector<vector<char>> ret;
+vector<vector<char>> m_board;
+    inline bool IsValid(int r, int c){
+        return !(r < 0 || r >= N || c < 0 || c>= M) && !visited[r][c] && m_board[r][c] == 'O';
     }
+    void BFS(int r, int c){
+        queue<pair<int, int>> q;
+        int dr[4] = {0, 1, 0, -1};
+        int dc[4] = {1, 0, -1, 0};
 
-    void DFS(int r, int c) {
+        q.push({r, c});
         visited[r][c] = true;
-        m_board[r][c] = 'O';
+        ret[r][c] = 'O';
 
-        for (int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-            if (IsValid(nr, nc)) {
-                DFS(nr, nc);
+        while(!q.empty()){
+            auto pos = q.front();
+            q.pop();
+
+            for(int i = 0 ; i < 4; i++){
+                int nr = pos.first + dr[i];
+                int nc = pos.second + dc[i];
+
+                if(IsValid(nr, nc)){
+                    visited[nr][nc] =true;
+                    ret[nr][nc] = 'O';
+                    q.push({nr, nc});
+                }
             }
         }
     }
 
     void solve(vector<vector<char>>& board) {
         N = board.size(), M = board[0].size();
+        m_board = board;
+        ret.resize(N, vector<char>(M, 'X'));
         visited.resize(N, vector<int>(M));
-        m_board.resize(N, vector<char>(M, 'X'));
-        g_board = board;
 
-        for (int c = 0; c < M; c++) {
-            if (IsValid(0, c)) {
-                DFS(0, c);
+        for(int r = 0 ; r < N; r++){
+            if(IsValid(r, 0)){
+                BFS(r, 0);
             }
-            if (IsValid(N - 1, c)) {
-                DFS(N - 1, c);
+            if(IsValid(r, M-1)){
+                BFS(r, M-1);
             }
         }
 
-        for (int r = 0; r < N; r++) {
-            if (IsValid(r, 0)) {
-                DFS(r, 0);
+        for(int c = 0 ; c < M; c++){
+            if(IsValid(0, c)){
+                BFS(0, c);
             }
-            if (IsValid(r, M-1)) {
-                DFS(r, M-1);
+            if(IsValid(N-1, c)){
+                BFS(N-1, c);
             }
         }
-        swap(m_board, board);
+        swap(board, ret);
     }
 };
