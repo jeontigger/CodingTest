213c9da9ba05e3bfe86f8967fdd1c6f8e7e284a0
jeontigger <boy9609@naver.com>
2025-12-11 00:34:47 +0900

    Time: 37 ms (15.94%), Space: 23.8 MB (23.09%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,44 +1,50 @@
 class Solution {
 public:
+    vector<vector<char>> m_grid;
+    int N, M;
     int dr[4] = {0, 1, 0, -1};
     int dc[4] = {1, 0, -1, 0};
-    int N, M;
-    vector<vector<char>> m_grid;
-    bool IsValid(int r, int c){
-        if(r < 0 || r >= N || c < 0 || c >= M)return false;
-        return m_grid[r][c] == '1';
+    bool IsValid(int r, int c) {
+        return !(r < 0 || r >= N || c < 0 || c >= M) && m_grid[r][c] == '1';
     }
 
-    int DFS(int r, int c) {
-        int ret = 1;
+    int BFS(int r, int c) {
+        m_grid[r][c] = '0';
+        queue<pair<int, int>> q;
+        q.push({r, c});
 
-        for(int i = 0 ; i < 4; i++){
-            int nr = r + dr[i];
-            int nc = c + dc[i];
+        int ret = 0;
+        while (!q.empty()) {
+            auto pos = q.front();
+            q.pop();
 
-            if(IsValid(nr, nc)){
-                m_grid[nr][nc] = '0';
-                ret += DFS(nr, nc);
+            ret++;
+            for (int i = 0; i < 4; i++) {
+                int nr = pos.first + dr[i];
+                int nc = pos.second + dc[i];
+
+                if (IsValid(nr, nc)) {
+                    m_grid[nr][nc] = '0';
+                    q.push({nr, nc});
+                }
             }
         }
 
         return ret;
     }
     int numIslands(vector<vector<char>>& grid) {
-        int ret = 0;
         m_grid = grid;
         N = grid.size(), M = grid[0].size();
 
+        int ret = 0;
         for(int i = 0 ; i < N; i++){
             for(int j = 0 ; j < M; j++){
                 if(IsValid(i, j)){
-                    m_grid[i][j] = '0';
-                    DFS(i, j);
+                    BFS(i, j);
                     ret++;
                 }
             }
         }
-
         return ret;
     }
 };
