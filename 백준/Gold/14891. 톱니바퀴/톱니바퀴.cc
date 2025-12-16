4422845822702d0c8a8943cdbb42fac4f173f784
jeontigger <boy9609@naver.com>
2025-12-16 22:34:34 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -12,6 +12,7 @@
 #include <cassert>
 #include <cstring>
 #include <list>
+#include <map>
 
 using namespace std;
 
@@ -31,102 +32,93 @@ void PrintVec(const vector<vector<T>>& vec) {
 		PrintVec(v);
 	}
 }
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
 
-// 극이 다르면 반대 방향으로 회전, 극이 같으면 회전 x
-// N극은 0, S극은 1. 12시부터 시계방향으로 주어짐
-// 회전 횟수 K, 1은 시계방향, -1은 반시계 방향
-// 총 점수는 12시 방향의 S극 개수와 유사
-
-// 톱니는 8개니까 회전을 8번하면 제자리
-// 회전 0번 기준으로 [2]번과 [6]번이 우측, 좌측을 뜻함
-// 회전 1번 기준으로 [1]번과 [5]번이 우측, 좌측
-// 회전 -1번 기준으로 [3]번과 [7]번이 우측, 좌측
-
-int gear[4][8];
+int gears[4][8];
+int idxs[4];
 int K;
-int rotNum[101];
-int rotDir[101];
-int rotCnt[4];
 
-void Inputs() {
-	for (int i = 0; i < 4; i++) {
-		string str;
-		cin >> str;
-		for (int j = 0; j < 8; j++) {
-			gear[i][j] = str[j] - '0';
+struct Command {
+	int idx;
+	int dir;
+};
+vector<Command> commands;
+
+bool Inputs() {
+	FOR(i, 4) {
+		FOR(j, 8) {
+			char ch;
+			cin >> ch;
+			gears[i][j] = ch - '0';
 		}
 	}
 
 	cin >> K;
-	for (int i = 0; i < K; i++) {
-		cin >> rotNum[i] >> rotDir[i];
-		rotNum[i]--;
-	}
-}
 
-void Rotate(int num, int dir) {
-	if (dir == 1) {
-		// 시계 방향
-		int end = gear[num][7];
-		for (int i = 7; i >= 1; i--) {
-			gear[num][i] = gear[num][i - 1];
-		}
-		gear[num][0] = end;
-	}
-	else {
-		// 반시계 방향
-		int begin = gear[num][0];
-		for (int i = 0; i < 7; i++) {
-			gear[num][i] = gear[num][i + 1];
-		}
-		gear[num][7] = begin;
-	}
-}
-
-#define LOG
-
-int GetLeft(int num) {
-	return gear[num][6];
-}
-
-int GetRight(int num) {
-	return gear[num][2];
-}
-
-void GearRotate(int num, int dir, vector<int>& visited) {
-	visited[num] = true;
-
-	// 왼쪽 회전
-	if (num > 0 && !visited[num - 1] && GetLeft(num) != GetRight(num - 1)) {
-		GearRotate(num - 1, -dir, visited);
+	FOR(i, K) {
+		int c, d;
+		cin >> c >> d;
+		commands.push_back({ c - 1, d });
 	}
 
-	// 오른쪽 회전
-	if (num < 3 && !visited[num + 1] && GetLeft(num + 1) != GetRight(num)) {
-		GearRotate(num + 1, -dir, visited);
-	}
+	memset(idxs, 0, sizeof(idxs));
 
-	Rotate(num, dir);
-
+	return true;
 }
 
-int GetScore() {
+int ConvertIdx(int idx) {
+	return (idx + 8) % 8;
+}
+
+void Rotate(int idx, int dir, vector<int>& visited) {
+	if (idx < 0 || idx >= 4) return;
+	if (visited[idx]) return;
+	visited[idx] = true;
+	int rightIdx = ConvertIdx(idxs[idx] + 2);
+	int leftIdx = ConvertIdx(idxs[idx] - 2);
+	if (idx > 0) {
+		int lrIdx = ConvertIdx(idxs[idx - 1] + 2);
+		if (gears[idx - 1][lrIdx] != gears[idx][leftIdx])
+			Rotate(idx - 1, -dir, visited);
+	}
+
+	if (idx < 3) {
+		int rlIdx = ConvertIdx(idxs[idx + 1] - 2);
+		if (gears[idx + 1][rlIdx] != gears[idx][rightIdx])
+			Rotate(idx + 1, -dir, visited);
+	}
+
+	idxs[idx] = ConvertIdx(idxs[idx] - dir);
+}
+
+int CalcScore() {
 	int ret = 0;
-
 	for (int i = 0; i < 4; i++) {
-		ret += gear[i][0] * pow(2, i);
+		int val = gears[i][idxs[i]];
+		if (val == 1) {
+			ret += pow(2, i);
+		}
 	}
-	// 0 => 0, 1 => 7, 2 =>6, 
 	return ret;
 }
 
+void PrintIdxs() {
+	for (int i = 0; i < 4; i++) {
+		cout << idxs[i] << ' ';
+	}
+	cout << '\n';
+}
+
 void Solution() {
+	//PrintIdxs();
 	for (int i = 0; i < K; i++) {
+		auto comm = commands[i];
 		vector<int> visited(4);
-		GearRotate(rotNum[i], rotDir[i], visited);
+		Rotate(comm.idx, comm.dir, visited);
+		//PrintIdxs();
 	}
 
-	cout << GetScore();
+	cout << CalcScore();
 }
 
 int main() {
@@ -138,10 +130,9 @@ int main() {
 	int T = 1;
 	//cin >> T;
 	while (T--) {
-		Inputs();
+		if (!Inputs()) break;
 		Solution();
 	}
-
 
 	return 0;
 }
