a8981b55f49376d04dbc9e0fe53c5b06988d8a71
jeontigger <boy9609@naver.com>
2026-01-21 21:47:20 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 32 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -8,6 +8,11 @@
 #include <unordered_map>
 #include <set>
 #include <sstream>
+#include <iomanip>
+#include <cassert>
+#include <cstring>
+#include <list>
+#include <map>
 
 using namespace std;
 
@@ -18,13 +23,47 @@ void PrintVec(const vector<T>& v) {
 	for (T i : v) {
 		cout << i << " ";
 	}
-	cout << endl;
+	cout << '\n';
 }
 
 template<typename T>
 void PrintVec(const vector<vector<T>>& vec) {
 	for (auto& v : vec) {
 		PrintVec(v);
+	}
+}
+#define FOR(idx, limit) for(int idx = 0; idx < limit; idx++)
+
+int N, M;
+int nums[100001];
+
+int c1[100001], c2[100001];
+
+bool Inputs() {
+	cin >> N >> M;
+	FOR(i, N) {
+		cin >> nums[i];
+	}
+	FOR(i, M) {
+		cin >> c1[i] >> c2[i];
+	}
+	return true;
+}
+
+int pSum[100002];
+
+void Init() {
+	pSum[0] = 0;
+	for (int i = 0; i < N; i++) {
+		pSum[i+1] = pSum[i] + nums[i];
+	}
+}
+
+void Solution() {
+	Init();
+
+	for (int i = 0; i < M; i++) {
+		cout << pSum[c2[i]] - pSum[c1[i] - 1] << '\n';
 	}
 }
 
@@ -34,24 +73,11 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
-	vector<int> nums(N);
-	for (int i = 0; i < N; i++) {
-		cin >> nums[i];
-	}
-
-	vector<int> table(N + 1);
-	table[0] = 0;
-	for (int i = 1; i < table.size(); i++) {
-		table[i] = table[i - 1] + nums[i - 1];
-	}
-
-	for (int i = 0; i < M; i++) {
-		int l, r;
-		cin >> l >> r;
-		cout << table[r] - table[l - 1] << ' ';
-
+	int T = 1;
+	//cin >> T;
+	while (T--) {
+		if (!Inputs()) break;
+		Solution();
 	}
 
 	return 0;
